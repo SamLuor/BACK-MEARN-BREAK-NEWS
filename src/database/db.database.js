e6ea5440d2ddf2bfb693a83ce580e819cf11cac1
SamLuor <samuelucasor@outlook.com>
2022-11-27 11:50:42 -0300

    criando variavel de ambiente e configurando dotenv

--- src/database/db.database.js
+++ src/database/db.database.js
@@ -4,10 +4,10 @@ const connectDatabase = () => {
   console.log("Wait connecting to the database");
 
   mongoose
-    .connect(
-      "mongodb+srv://Kempachi39:H3bBrmzC7kTjALzi@cluster0.pvi8f6x.mongodb.net/?retryWrites=true&w=majority",
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log("MongoDB Atlas Connected"))
     .catch((error) => console.log(error));
 };
